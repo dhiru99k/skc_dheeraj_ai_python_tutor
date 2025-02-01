7de51493d19ac63b0604c5e19bc6a874884f7df5
dhiru99k <dk5333341@gmail.com>
2025-02-01 17:58:39 +0530

    Updated json

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: "/skc_dheeraj_ai_python_tutor/",
   plugins: [
     tailwindcss(),
   ],
